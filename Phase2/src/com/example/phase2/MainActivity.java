aac450855b7360c0f80a7e8f030bf87508bd4b2b
rbk2kb <rbk2kb@virginia.edu>
2014-03-17 00:40:07 -0400

    Added a switch for filtering the buddylist by friends who are online

--- Phase2/src/com/example/phase2/MainActivity.java
+++ Phase2/src/com/example/phase2/MainActivity.java
@@ -3,16 +3,32 @@ package com.example.phase2;
 import android.os.Bundle;
 import android.app.Activity;
 import android.view.Menu;
+import android.widget.CompoundButton;
+import android.widget.CompoundButton.OnCheckedChangeListener;
+import android.widget.Switch;
 
 public class MainActivity extends Activity {
 	private BuddyListFragment buddyList;
-	
+	private Switch onlineSwitch;
 	
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         buddyList = (BuddyListFragment) getFragmentManager().findFragmentById(R.id.main_buddy_list);
+        onlineSwitch = (Switch) findViewById(R.id.online_friends_switch);
+        onlineSwitch.setOnCheckedChangeListener(new OnCheckedChangeListener() {
+
+			@Override
+			public void onCheckedChanged(CompoundButton view, boolean isChecked) {
+				if(isChecked) {
+					buddyList.filterListByOnline(true);
+				} else {
+					buddyList.populateFullList();
+				}
+			}
+        	
+        });
         buddyList.addTestData();
     }
 
