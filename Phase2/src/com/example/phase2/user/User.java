21d029b5549bbea649edaaf38b44db778eb38a4b
ta3fh <ta3fh@virginia.edu>
2014-03-18 19:59:57 -0400

    Added code to get output of web service

--- Phase2/src/com/example/phase2/user/User.java
+++ Phase2/src/com/example/phase2/user/User.java
@@ -1,5 +1,11 @@
 package com.example.phase2.user;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
 import java.util.ArrayList;
 import java.util.Collections;
 
@@ -11,8 +17,36 @@ public class User {
 	private static ArrayList<Buddy> buddies = new ArrayList<Buddy>();
 	private static boolean loggedIn = true;
 	
+	// currently just prints the output of the web service with the parameter user-id 2
 	public static void requestBuddiesFromServer() {
-		
+		try {
+			URL url = new URL("http://plato.cs.virginia.edu/~cs4720s14beans/api/users/friends/2");
+			HttpURLConnection con = (HttpURLConnection) url.openConnection();
+			readStream(con.getInputStream());
+		} catch(Exception e) {
+			e.printStackTrace();
+		}
+	}
+	
+	public static void readStream(InputStream input) {
+		BufferedReader reader = null;
+		try {
+			reader = new BufferedReader(new InputStreamReader(input));
+			String line = "";
+			while ((line = reader.readLine()) != null) {
+				System.out.println(line);
+			}
+		} catch(IOException e) {
+			e.printStackTrace();
+		} finally {
+			if (reader != null) {
+				try {
+					reader.close();
+				} catch(IOException e) {
+					e.printStackTrace();
+				}
+			}
+		}
 	}
 	
 	public static ArrayList<Buddy> getBuddies() {
